1e9eef0b4a67e6c4a3cbc1a924e76f1b92667aa5
JuRyunn <79950504+JuRyunn@users.noreply.github.com>
2022-08-13 23:44:28 +0900

    FirstCommit

---
+++ truffle-config.js
@@ -0,0 +1,24 @@
+require('bable-register');
+require('bable-polyfill');
+
+module.export= {
+    networks: {
+        development: {
+            host:'127.0.0.1:',
+            port: '7545', 
+            network_id: '*' // match any network
+        },
+    },
+
+    contracts_directory: './src/contracts',
+    contracts_build_directory: './src/truffle_abis',
+    compilers: {
+        solc: {
+            version: '^0.5.0',
+            optimizer: {
+                enabled: true,
+                runs: 200
+            },
+        }
+    }
+}
